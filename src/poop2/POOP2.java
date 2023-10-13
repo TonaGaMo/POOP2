56d92f5feca128115b41a2dabc0d4a25a7256d33
TonaGaMo <111716034+TonaGaMo@users.noreply.github.com>
2023-10-12 20:31:27 -0600

    Creación de practica 2

---
+++ src/poop2/POOP2.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop2;
+
+/**
+ *
+ * @author Asus
+ */
+public class POOP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+        
+        
+        System.out.println("Suma y resta de dos numeros");
+        //Declaramos las variables para hacer la suma 
+ int a;
+ a = 10;
+ int b;
+ b= 5;
+ int c;
+ c = a+ b;
+ int d;
+ d = a - b;
+ // impriminos utilizando el Sout con las variables ya con su respectiva operacion 
+ System.out.println("La suma es: "+ c);
+ System.out.println("La resta es: "+ d);
+ //Hacemos el uso de un if para hacer la comparacion de dos numeros 
+ System.out.println("Comparacion de dos numeros");
+ if(c < d){
+ System.out.println("C es menor que b");
+ }
+ else{
+ System.out.println("D es mayor que c");
+ }
+ //Aqui utlizamos el float para hacer la suma de dos numeros flotantes 
+ System.out.println("Ahora suma de dos numeros flotantes");
+ float w;
+ float x;
+ w = (float) 5.25;
+ x = (float) 10.25;
+ 
+ float z = w + x;
+ System.out.println( " La suma de los flotantes es:" + z); 
+ 
+ }
+ 
+}
+
+        
+    
+    
+
